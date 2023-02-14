a791cf0fc4b92d3b8a42045683e40684989e98d2
jejehyunlee <jefrisaputra989@gmail.com>
2023-02-14 11:16:55 +0700

    ADD_Cateeory_Product

---
+++ src/main/java/com/juaracoding/jsspringbootjpa/model/CategoryProduct.java
@@ -0,0 +1,70 @@
+package com.juaracoding.jsspringbootjpa.model;
+
+/*
+Created By IntelliJ IDEA 2022.1.3 (Community Edition)
+Build #IC-221.5921.22, built on June 21, 2022
+@Author JEJE a.k.a Jefri S
+Java Developer
+Created On 2/14/2023 03:14
+@Last Modified 2/14/2023 03:14
+Version 1.0
+*/
+
+
+import com.juaracoding.jsspringbootjpa.utils.ConstantMessage;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table (name = "MstCategoryProduct")
+public class CategoryProduct extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IDCategoryProduct", nullable = false)
+    private Long idCategoryProduct;
+
+    @Column(name = "NameCategoryProduct", nullable = false, length = 40)
+    @Length(message = ConstantMessage.MAX_CHAR_NAME_CATPROD, max = 40)
+    @NotEmpty(message = ConstantMessage.CHAR_NAME_NOTEMPY)
+    private String nameCategoryProduct;
+
+    @Column(name = "DescCategoryProduct", nullable = false, length = 500)
+    @NotEmpty(message = ConstantMessage.CHAR_DESC_NOTEMPTY)
+    private String descCategoryProduct;
+
+    public CategoryProduct() {
+    }
+
+    public CategoryProduct(Long idCategoryProduct, String nameCategoryProduct, String descCategoryProduct) {
+        this.idCategoryProduct = idCategoryProduct;
+        this.nameCategoryProduct = nameCategoryProduct;
+        this.descCategoryProduct = descCategoryProduct;
+    }
+
+    public Long getIdCategoryProduct() {
+        return idCategoryProduct;
+    }
+
+    public void setIdCategoryProduct(Long idCategoryProduct) {
+        this.idCategoryProduct = idCategoryProduct;
+    }
+
+    public String getNameCategoryProduct() {
+        return nameCategoryProduct;
+    }
+
+    public void setNameCategoryProduct(String nameCategoryProduct) {
+        this.nameCategoryProduct = nameCategoryProduct;
+    }
+
+    public String getDescCategoryProduct() {
+        return descCategoryProduct;
+    }
+
+    public void setDescCategoryProduct(String descCategoryProduct) {
+        this.descCategoryProduct = descCategoryProduct;
+    }
+}
+
