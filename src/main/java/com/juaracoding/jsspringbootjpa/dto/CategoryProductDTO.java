70aa0a7c07b567aabb9cabdc44ea39fc731f30dc
jejehyunlee <jefrisaputra989@gmail.com>
2023-02-19 00:02:01 +0700

    Add CATPROD_DTo

---
+++ src/main/java/com/juaracoding/jsspringbootjpa/dto/CategoryProductDTO.java
@@ -0,0 +1,42 @@
+package com.juaracoding.jsspringbootjpa.dto;
+
+/*
+Created By IntelliJ IDEA 2022.1.3 (Community Edition)
+Build #IC-221.5921.22, built on June 21, 2022
+@Author JEJE a.k.a Jefri S
+Java Developer
+Created On 2/17/2023 08:42
+@Last Modified 2/17/2023 08:42
+Version 1.0
+*/
+
+public class CategoryProductDTO {
+    private Long idCategoryProduct;
+    private String nameCategoryProduct;
+    private String strDescCategoryProduct;
+
+
+    public Long getIdCategoryProduct() {
+        return idCategoryProduct;
+    }
+
+    public void setIdCategoryProduct(Long idCategoryProduct) {
+        this.idCategoryProduct = idCategoryProduct;
+    }
+
+    public String getNameCategoryProduct() {
+        return nameCategoryProduct;
+    }
+
+    public void setNameCategoryProduct(String nameCategoryProduct) {
+        this.nameCategoryProduct = nameCategoryProduct;
+    }
+
+    public String getStrDescCategoryProduct() {
+        return strDescCategoryProduct;
+    }
+
+    public void setStrDescCategoryProduct(String strDescCategoryProduct) {
+        this.strDescCategoryProduct = strDescCategoryProduct;
+    }
+}
