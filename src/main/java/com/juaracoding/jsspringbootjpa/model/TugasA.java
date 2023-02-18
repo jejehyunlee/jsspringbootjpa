e1920c239a306c2d78dbdebcd309b37b4b2de76b
jejehyunlee <jefrisaputra989@gmail.com>
2023-02-19 00:00:58 +0700

    Add TugasA

---
+++ src/main/java/com/juaracoding/jsspringbootjpa/model/TugasA.java
@@ -0,0 +1,145 @@
+package com.juaracoding.jsspringbootjpa.model;
+
+/*
+Created By IntelliJ IDEA 2022.1.3 (Community Edition)
+Build #IC-221.5921.22, built on June 21, 2022
+@Author JEJE a.k.a Jefri S
+Java Developer
+Created On 2/18/2023 23:21
+@Last Modified 2/18/2023 23:21
+Version 1.0
+*/
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "MstTugasA")
+public class TugasA {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IDTugasA", nullable = false)
+    private Long idTugasA;
+
+    @Column(name = "Nama", nullable = false, length = 40)
+    private String nama;
+
+    @Column(name = "Alamat", nullable = false, length = 500)
+    private String alamat;
+
+    @Column(name = "TanggalLahir", nullable = false)
+    private Date tanggalLahir;
+
+    @Column(name = "JenisKelamin", nullable = false)
+    private Character jenisKelamin;
+
+    @Column(name = "MasihHidup", nullable = false)
+    private Boolean masihHidup;
+
+
+    // Start Of auditTrail
+    @Column(name = "CreatedDate",nullable = false)
+    private Date createdDate = new Date();
+
+    @Column(name = "CreatedBy", nullable = false)
+    private Integer createdBy;
+
+    @Column(name = "ModifiedDate")
+    private Date modifiedDate;
+
+    @Column(name = "ModifiedBy")
+    private Integer modifiedBy;
+
+    @Column ( name = "IsDeleted", nullable = false )
+    private Byte isDeleted = 1;
+    // End Of AuditTrail
+
+
+    public Long getIdTugasA() {
+        return idTugasA;
+    }
+
+    public void setIdTugasA(Long idTugasA) {
+        this.idTugasA = idTugasA;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public Character getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(Character jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public Boolean getMasihHidup() {
+        return masihHidup;
+    }
+
+    public void setMasihHidup(Boolean masihHidup) {
+        this.masihHidup = masihHidup;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public Integer getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(Integer modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Byte getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Byte isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
