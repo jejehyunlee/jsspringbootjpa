a6c5b0982ee5314a42cb4770b8f5b626765fa0ed
jejehyunlee <jefrisaputra989@gmail.com>
2023-02-19 18:43:38 +0700

    Empty

--- src/main/java/com/juaracoding/jsspringbootjpa/model/BaseEntity.java
+++ src/main/java/com/juaracoding/jsspringbootjpa/model/BaseEntity.java
@@ -1,104 +1,57 @@
-package com.juaracoding.jsspringbootjpa.model;
-
-/*
-Created By IntelliJ IDEA 2022.1.3 (Community Edition)
-Build #IC-221.5921.22, built on June 21, 2022
-@Author JEJE a.k.a Jefri S
-Java Developer
-Created On 2/12/2023 00:53
-@Last Modified 2/12/2023 00:53
-Version 1.0
-*/
-
+//package com.juaracoding.jsspringbootjpa.model;
 //
-//import org.springframework.data.annotation.CreatedBy;
-//import org.springframework.data.annotation.CreatedDate;
-//import org.springframework.data.annotation.LastModifiedBy;
-//import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import javax.persistence.*;
-import java.util.Date;
-
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
-
-    @Column(name = "CreatedDate",nullable = false)
-    private Date createdDate = new Date();
-
-    @Column(name = "CreatedBy", nullable = false)
-    private Integer createdBy;
-
-    @Column(name = "ModifiedDate")
-    private Date modifiedDate;
-
-    @Column(name = "ModifiedBy")
-    private Integer modifiedBy;
-
-    @Column ( name = "IsDeleted", nullable = false )
-    private Byte isDeleted = 1;
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Integer getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(Integer createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Date getModifiedDate() {
-        return modifiedDate;
-    }
-
-    public void setModifiedDate(Date modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
-
-    public Integer getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(Integer modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
-
-    public Byte getIsDeleted() {
-        return isDeleted;
-    }
-
-    public void setIsDeleted(Byte isDeleted) {
-        this.isDeleted = isDeleted;
-    }
-
-    //
-//    @CreatedBy
-//    @Column(name = "CreatedBy")
-//    private String createdBy;
-
-//    @CreatedDate
-//    @Temporal(TemporalType.TIMESTAMP)
-//    @Column( name = "CreatedDate", nullable = false )
-//    private Date createdDate = new Date();
-
-//    @LastModifiedBy
-//    @Column(name = "UpdateBy")
-//    private String updateBy;
-
-//    @LastModifiedDate
-//    @Temporal(TemporalType.TIMESTAMP)
-//    @Column ( name = "UpdatedDate" )
-//    private Date updatedDate;
-
-
-
-
-}
-
+///*
+//Created By IntelliJ IDEA 2022.1.3 (Community Edition)
+//Build #IC-221.5921.22, built on June 21, 2022
+//@Author JEJE a.k.a Jefri S
+//Java Developer
+//Created On 2/12/2023 00:53
+//@Last Modified 2/12/2023 00:53
+//Version 1.0
+//*/
+//
+////
+////import org.springframework.data.annotation.CreatedBy;
+////import org.springframework.data.annotation.CreatedDate;
+////import org.springframework.data.annotation.LastModifiedBy;
+////import org.springframework.data.annotation.LastModifiedDate;
+//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+//import javax.persistence.*;
+//import java.util.Date;
+//
+//@MappedSuperclass
+//@EntityListeners(AuditingEntityListener.class)
+//public class BaseEntity {
+//
+//    public Byte getIsDeleted() {
+//        return isDeleted;
+////    }
+//
+//    public void setIsDeleted(Byte isDeleted) {
+//        this.isDeleted = isDeleted;
+//    }
+//
+//    //
+////    @CreatedBy
+////    @Column(name = "CreatedBy")
+////    private String createdBy;
+//
+////    @CreatedDate
+////    @Temporal(TemporalType.TIMESTAMP)
+////    @Column( name = "CreatedDate", nullable = false )
+////    private Date createdDate = new Date();
+//
+////    @LastModifiedBy
+////    @Column(name = "UpdateBy")
+////    private String updateBy;
+//
+////    @LastModifiedDate
+////    @Temporal(TemporalType.TIMESTAMP)
+////    @Column ( name = "UpdatedDate" )
+////    private Date updatedDate;
+//
+//
+//
+//
+//}
+//
